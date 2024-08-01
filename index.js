82fa8551bf0463b8ed21bb9a2dd231df20332757
Brittney Epperson <beppers87@hotmail.com>
2024-08-01 16:15:10 +0000

    Added and connected to mongoose

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require("mongoose");
+const dns = require("dns");
+const urlparser = require("url");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB is connected..."))
+  .catch((err) => console.log(err));
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
